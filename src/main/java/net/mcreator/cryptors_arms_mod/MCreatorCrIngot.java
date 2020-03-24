4f132bc9ee6a1841a9fc1faba80f14f2b3b623a9
WannaCryptor <34532755+WannaCryptor@users.noreply.github.com>
2020-03-24 18:45:06 +1000

    Crafts added

---
+++ src/main/java/net/mcreator/cryptors_arms_mod/MCreatorCrIngot.java
@@ -0,0 +1,58 @@
+package net.mcreator.cryptors_arms_mod;
+
+import net.minecraftforge.fml.relauncher.SideOnly;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.client.event.ModelRegistryEvent;
+
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.block.state.IBlockState;
+
+@Elementscryptors_arms_mod.ModElement.Tag
+public class MCreatorCrIngot extends Elementscryptors_arms_mod.ModElement {
+	@GameRegistry.ObjectHolder("cryptors_arms_mod:cringot")
+	public static final Item block = null;
+
+	public MCreatorCrIngot(Elementscryptors_arms_mod instance) {
+		super(instance, 17);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new ItemCustom());
+	}
+
+	@SideOnly(Side.CLIENT)
+	@Override
+	public void registerModels(ModelRegistryEvent event) {
+		ModelLoader.setCustomModelResourceLocation(block, 0, new ModelResourceLocation("cryptors_arms_mod:cringot", "inventory"));
+	}
+
+	public static class ItemCustom extends Item {
+		public ItemCustom() {
+			setMaxDamage(1);
+			maxStackSize = 64;
+			setUnlocalizedName("cringot");
+			setRegistryName("cringot");
+			setCreativeTab(MCreatorModTab.tab);
+		}
+
+		@Override
+		public int getItemEnchantability() {
+			return 0;
+		}
+
+		@Override
+		public int getMaxItemUseDuration(ItemStack itemstack) {
+			return 0;
+		}
+
+		@Override
+		public float getDestroySpeed(ItemStack par1ItemStack, IBlockState par2Block) {
+			return 1F;
+		}
+	}
+}
